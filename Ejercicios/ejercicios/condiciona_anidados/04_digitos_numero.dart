d825177ed8d2579774587e228af0361bc8c93121
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-16 00:39:53 -0500

    Ejercicio 8 condicional: digito de numeros pero que no pase de 4

---
+++ Ejercicios/ejercicios/condiciona_anidados/04_digitos_numero.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+main () {
+  stdout.writeln('Ingresa un numero positivo del 1 al 999');
+  int number = int.parse(stdin.readLineSync() ?? '0');
+
+  if(number > 999){
+    throw 'El numero excede el limite de digitos';
+  }
+
+  if (number > 99){
+    print('El numero tiene tres digitos');
+    return;
+  }
+
+  if (number > 9) {
+    print('El numero tiene dos digitos');
+    return;
+  }
+
+  print('El numero tiene un solo digito');
+}
