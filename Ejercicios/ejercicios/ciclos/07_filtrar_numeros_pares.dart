28dad8570751b5e90da5e0468bbfd0bd2fdd7149
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-17 15:47:39 -0500

    Ejercicio 21 ciclo for in: filtro de numeros pares

---
+++ Ejercicios/ejercicios/ciclos/07_filtrar_numeros_pares.dart
@@ -0,0 +1,14 @@
+import '../shared/utils/read_and_write.dart';
+
+main() {
+  List<int> numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<int> numerosPares = [];
+  for (int numero in numeros) {
+    if (numero % 2 != 0) continue;
+    numerosPares.add(numero);
+  }
+
+  for (int par in numerosPares) {
+    ReadAndWrite.writeConsole('$par');
+  }
+}
