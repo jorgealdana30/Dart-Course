cd4ae6fdbebc6ff7630ba7000d6052a95a9768d7
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-17 14:07:53 -0500

    Ejercicio 18 ciclo for: tablas de multiplicar

---
+++ Ejercicios/ejercicios/ciclos/03_tablas_multiplicar.dart
@@ -0,0 +1,10 @@
+import '../shared/utils/read_and_write.dart';
+
+main () {
+  ReadAndWrite.writeConsole('Tablas de multiplicar: ');
+  int number = ReadAndWrite.readNumber('Ingresa un numero: ');
+
+  for(int i = 1; i <= 12; i++) {
+    ReadAndWrite.writeConsole('$number * $i = ${number * i}');
+  }
+}
