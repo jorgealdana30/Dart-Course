c901726d6ac16cc157e41df26655dfa2461b75c8
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-17 15:59:00 -0500

    Ejercicio 23 ciclo for in: lista de numeros cuadrados

---
+++ Ejercicios/ejercicios/ciclos/09_lista_cuadrados.dart
@@ -0,0 +1,13 @@
+import '../shared/utils/read_and_write.dart';
+
+main() {
+  List<int> numbers = [2, 3, 4, 5, 6];
+  List<int> squaredList = [];
+  for (int number in numbers) {
+    squaredList.add(number * number);
+  }
+
+  for (int squared in squaredList) {
+    ReadAndWrite.writeConsole('$squared');
+  }
+}
