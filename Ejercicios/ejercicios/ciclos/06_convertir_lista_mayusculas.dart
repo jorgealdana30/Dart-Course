144b67a13629984a374077afeee2a81925c728fb
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-17 15:44:09 -0500

    Ejercicio 20 ciclo for in: palabras mayusculas

---
+++ Ejercicios/ejercicios/ciclos/06_convertir_lista_mayusculas.dart
@@ -0,0 +1,13 @@
+import '../shared/utils/read_and_write.dart';
+
+main() {
+  List<String> palabras = ['hola', 'mundo', 'dart', 'flutter'];
+  List<String> palabrasMayus = [];
+  for (String palabra in palabras) {
+    palabrasMayus.add(palabra.toUpperCase());
+  }
+
+  for (String mayus in palabrasMayus) {
+    ReadAndWrite.writeConsole(mayus);
+  }
+}
