e37e7d0cb7de444dc2e689cbcf69e854238a9a96
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-17 15:51:33 -0500

    Ejercicio 22 ciclo for in: cantidad palabras mayores a 5

---
+++ Ejercicios/ejercicios/ciclos/08_contar_palabras.dart
@@ -0,0 +1,14 @@
+import '../shared/utils/read_and_write.dart';
+
+main() {
+  List<String> words = ['elefante', 'gato', 'cocodrilo', 'pez', 'hipopótamo'];
+  int countWordsMoreThanFive = 0;
+
+  for (String word in words) {
+    if (word.length < 5) continue;
+    countWordsMoreThanFive += 1;
+  }
+
+  ReadAndWrite.writeConsole(
+      'Cantidad palabras con más de 5 letras: $countWordsMoreThanFive');
+}
