4cadaf69005ae0eaab95ec6e81f9a145b70e5996
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-21 12:20:46 -0500

    Ejercicio 28 switch: calificacion de notas

---
+++ Ejercicios/ejercicios/switch/01_evaluacion_notas.dart
@@ -0,0 +1,30 @@
+import '../shared/utils/read_and_write.dart';
+
+main() {
+  String nota = ReadAndWrite.readString('Ingrese la calificación (A, B, C, D, E)');
+
+  switch (nota) {
+    case 'A':
+      ReadAndWrite.writeConsole('Excelente!');
+      break;
+
+    case 'B':
+      ReadAndWrite.writeConsole('Bueno!');
+      break;
+
+    case 'C':
+      ReadAndWrite.writeConsole('Regular');
+      break;
+
+    case 'D':
+      ReadAndWrite.writeConsole('Deficiente');
+      break;
+
+    case 'E':
+      ReadAndWrite.writeConsole('Insuficiente');
+      break;
+
+    default:
+      ReadAndWrite.writeConsole('Nota no valida');
+  }
+}
