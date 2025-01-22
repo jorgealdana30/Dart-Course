eee7e768198f03941d23fced2f0aa94bb360f1f9
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-22 16:48:15 -0500

    Ejercicio 31 funciones: concatenando cadenas

---
+++ Ejercicios/ejercicios/funciones/02_concatenar_cadenas.dart
@@ -0,0 +1,12 @@
+import '../shared/utils/read_and_write.dart';
+
+void main() {
+  String cadena1 = ReadAndWrite.readString('Ingrese primera frase');
+  String cadena2 = ReadAndWrite.readString('Ingrese segunda frase');
+
+  print(concatenarCadenas(cadena1, cadena2));
+}
+
+String concatenarCadenas(String cadena1, String cadena2) {
+  return '$cadena1 $cadena2';
+}
