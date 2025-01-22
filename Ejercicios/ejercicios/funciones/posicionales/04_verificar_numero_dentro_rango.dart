8199c38b2cb7dce12d92c21f47c910fce2330aa9
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2025-01-22 16:53:43 -0500

    Ejercicio 33 funciones: minimo y maximo

---
+++ Ejercicios/ejercicios/funciones/posicionales/04_verificar_numero_dentro_rango.dart
@@ -0,0 +1,14 @@
+import '../../shared/utils/read_and_write.dart';
+
+void main() {
+
+  int number = ReadAndWrite.readNumber('Ingresa un numero');
+  int min = ReadAndWrite.readNumber('Ingresa un numero minimo');
+  int max = ReadAndWrite.readNumber('Ingresa un numero maximo');
+
+  print(estaEnRango(number, min, max) ? 'El numero está en rango' : 'El numero NO está en rango');
+}
+
+bool estaEnRango(int number, int min, int max) {
+  return number >= min && number <= max;
+}
