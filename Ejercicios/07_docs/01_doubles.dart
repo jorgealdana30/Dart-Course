3309d158b63dc6b30b40dc099d56e392f700bc62
OJA01659 <jorge.aldana.ext@itau.co>
2025-02-18 18:52:39 -0500

    Ejercicio 51 ejercicios de docs: double

---
+++ Ejercicios/07_docs/01_doubles.dart
@@ -0,0 +1,11 @@
+main() {
+
+  double numero = 3.1416;
+
+  print('Firma: ${numero.sign} :: ${numero}');
+  print('isFinite: ${numero.isFinite} :: ${numero}');
+  print('isInfinite: ${numero.isInfinite} :: ${numero}');
+  print('abs: ${numero.abs()} :: ${numero}');
+  print('ceil: ${numero.ceilToDouble()} :: ${numero}');
+  print('round: ${numero.roundToDouble()} :: ${numero}');
+}
